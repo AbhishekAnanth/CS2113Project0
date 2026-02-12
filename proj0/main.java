aba7547d5cb8f51e9a5b8dc6edd36fab15f547cc
sillyybilly <128650755+sillyybilly@users.noreply.github.com>
2026-02-12 14:56:11 -0600

    Update main.java

--- proj0/main.java
+++ proj0/main.java
@@ -2,27 +2,29 @@
 public class main {
     // To Do: make variables & arrays + do the menu options w/ while loop (with option to exit)
 
-    final int MAX_VALUE = 50;
-    int item;
-    int choice; // User choice in Menu
-    boolean checkOff; 
+    static final int MAX_VALUE = 50;
 
-    // 1 = Add Item
-    // 2 = Remove Item 
-    // 3 = Check Off Item
-    // 4 = Print Grocery List
-    // 5 = Exit
-
-    while(choice != 5){
+    public static void main(String[] args){
+        int item;
+        int choice; // User choice in Menu
+        boolean checkOff; 
+    
+        // 1 = Add Item
+        // 2 = Remove Item 
+        // 3 = Check Off Item
+        // 4 = Print Grocery List
+        // 5 = Exit
+    
+        while(choice != 5){
     
     }
-    
-
 
     // Add Method
     // Remove Method
     // Check Off Method 
     // Print List Method
     // Finding Item Method 
+        
+    }
     
 }
