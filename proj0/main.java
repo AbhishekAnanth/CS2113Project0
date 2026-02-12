57a12f11fba05a9d3df9a70834a8c49b88470782
AbhishekAnanth <123286124+AbhishekAnanth@users.noreply.github.com>
2026-02-12 20:57:27 +0000

    rough outline committ

--- proj0/main.java
+++ proj0/main.java
@@ -1,4 +1,51 @@
 
 public class main {
-    
+
+    static String[] items = new String[50];
+    static Boolean[] checked = new Boolean[50];
+
+    static int itemCount = 0;
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        boolean running = true;
+
+        while (running) {
+            printMenu();
+
+            //TODO: read menu choices
+
+            int choice = -1;
+            
+            switch (choice) {
+                case 1:
+                    //call addItem
+                    break;
+                
+                case 2:
+                    //call removeItem
+                    break;
+
+                case 3:
+                    //call checkOffItem
+                    break;
+
+                case 4:
+                    //call printList
+                    break;
+
+                case 5:
+                    //print final list 
+                    //goodbye message
+                    //set running to false
+                    break;
+
+                default:
+                    //make sure menu options are valid
+            }
+
+        }
+    }
+
 }
